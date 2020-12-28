7396ec57c91e5585a808fc3d665afe3c7df64ff3
Rodrigo Ferreira <rdferreira90@gmail.com>
2020-12-28 14:49:59 -0300

    Criando operação de instanciação

--- src/main/java/com/rodrigoferreira/springapi/SpringApiApplication.java
+++ src/main/java/com/rodrigoferreira/springapi/SpringApiApplication.java
@@ -1,13 +1,33 @@
 package com.rodrigoferreira.springapi;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.rodrigoferreira.springapi.domain.Categoria;
+import com.rodrigoferreira.springapi.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class SpringApiApplication {
+public class SpringApiApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringApiApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
